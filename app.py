5adf45927af8c8489ac07feb1872224bacd39fdc
Penumalle Jaya Naga Rajeswari <166640947+Rajeswaril4@users.noreply.github.com>
2026-02-12 00:54:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def log_request_info():
 CORS(app, 
      supports_credentials=True,
      origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", 
-              "http://127.0.0.1:5173", "http://127.0.0.1:5174", "http://127.0.0.1:3000"],
+              "http://127.0.0.1:5173", "http://127.0.0.1:5174", "http://127.0.0.1:3000", "*"],
      allow_headers=["Content-Type", "Authorization"],
      methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 
